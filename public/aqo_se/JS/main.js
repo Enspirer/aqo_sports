f8d0e0d638c63c110c139de96ad3bc4f58f4531d
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-02-03 16:35:43 +0530

    AQO SE Updates Styles

--- public/aqo_se/JS/main.js
+++ public/aqo_se/JS/main.js
@@ -25,6 +25,12 @@ $(document).ready(function () {
     }, 400);
     return false;
   });
+  // Avatar Drop Down 
+  document.querySelector('.mini-photo-wrapper').addEventListener('click', function() {
+    document.querySelector('.menu-container').classList.toggle('active');
+  });
+
+ 
 });
 
 // Mobile Navigation
@@ -47,6 +53,34 @@ $(function() {
   });
 
 
+  // time countdown
+  const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+const date = "11 feb 2021";
+function countdown() {
+  const newYearsDate = new Date(date);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.innerHTML = formatTime(days);
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+}
+function formatTime(time) {
+  return time < 10 ? `0${time}`:time;
+}
+countdown();
+setInterval(countdown, 1000);
 
 // Navigtion 
 var elem = $('.nav-item');
@@ -93,3 +127,6 @@ elem.each(function(i){
 $('.nav').mouseleave(function() {
   index = undefined;
 });
+
+
+
