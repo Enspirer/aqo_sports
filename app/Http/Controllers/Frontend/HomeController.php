3233df069d1b0a62d4ac08a2c92eeddd9ff6f7ed
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-02-03 16:16:59 +0530

    Home Controller

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use Modules\Competition\Entities\CompetitionCategory;
+use Modules\Competition\Entities\Competition;
 
 /**
  * Class HomeController.
@@ -14,6 +16,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('frontend.index');
+        $competitionCategory = CompetitionCategory::all();
+        $trendingCompetition = Competition::where('is_feature', 1)->get();
+        return view('frontend.index',[
+            'competitionCategory' => $competitionCategory,
+            'trendingCompetition' => $trendingCompetition
+        ]);
+
     }
 }
