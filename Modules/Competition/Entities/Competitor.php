0580f847179d8d84effd808d2f88300840b202d5
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-02-04 09:25:18 +0530

    Competitor Shows

--- Modules/Competition/Entities/Competitor.php
+++ Modules/Competition/Entities/Competitor.php
@@ -5,6 +5,7 @@ namespace Modules\Competition\Entities;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+
 class Competitor extends Model
 {
 
@@ -13,5 +14,17 @@ class Competitor extends Model
     protected static function newFactory()
     {
         return \Modules\Competition\Database\factories\CompetitorFactory::new();
+    }
+
+    public static function IsAppliedCompetition($competition_id)
+    {
+        if (auth()->check()) {
+            $competitorDetails = Competitor::where('competition_id',$competition_id)
+                ->where('user_id',auth()->user()->id)
+                ->first();
+            return $competitorDetails;
+        }else{
+            return null;
+        }
     }
 }
