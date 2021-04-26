a1316004e44f27c6bc1c3d4ede59c20526b87dc8
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-04-26 18:59:43 +0530

    Create Event Controller

--- Modules/Competition/Http/Controllers/Frontend/CreateEvenetController.php
+++ Modules/Competition/Http/Controllers/Frontend/CreateEvenetController.php
@@ -27,11 +27,84 @@ class CreateEvenetController extends Controller
         ]);
     }
 
+    public function orz_create_competition_store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'competition_name' => 'required:max:255',
+            'competition_description' => 'required',
+            'is_feature' => 'required',
+            'category' => 'required',
+            'payment_type' => 'required',
+            'status' => 'required',
+            'start_date' => 'required',
+            'end_date' => 'required',
+        ]);
+        $competition = new Competition;
+        $competition->competition_name = $request->competition_name;
+        $competition->description = $request->competition_description;
+        $competition->is_feature = $request->is_feature;
+        if($request->file('feature_image'))
+        {
+            //Feature Images
+            $imageName = time().'.'.$request->feature_image->getClientOriginalExtension();
+            $fullURLs = $request->feature_image->move(public_path('files'), $imageName);
+            $competition->feature_image = $imageName;
+        }else{
+            $competition->feature_image = 'no_img.jpg';
+        }
+        $competition->user_id = auth()->user()->id;
+        $competition->category_id = $request->category;
+        $competition->payment_type = $request->payment_type;
+        $competition->status = $request->status;
+        $competition->started_date = $request->start_date;
+        $competition->end_date = $request->end_date;
+        $competition->register_form = $request->register_form_data;
+        $competition->marks_sections = json_encode($request->marks_sections);
+        $competition->rounds_section = json_encode($request->rounds_section);
+
+        //Game Rules
+        $ruleNames= $request->rule_name;
+        $ruleDescriptions = $request->description_rule;
+        $outArray =[];
+        if($ruleNames != null)
+        {
+            foreach ($ruleNames as $key=>$ruleName){
+                $outputArray = [
+                    'rule_name' => $ruleName,
+                    'rule_description' =>$ruleDescriptions[$key]
+                ];
+                array_push($outArray,$outputArray);
+            }
+        }
+
+        $jsonOutput = json_encode($outArray);
+        $competition->game_rules = $jsonOutput;
+        $competition->save();
+        return back();
+    }
+
     public function create_competition()
     {
         $getCategory = CompetitionCategory::all();
         return view('frontend.user.orgz_create_competition',[
             'get_category' => $getCategory,
+        ]);
+    }
+
+
+    public function orz_edit_competition($id)
+    {
+        $getCompetionDetaills = Competition::where('id',$id)->first();
+        $getGameRules = json_decode($getCompetionDetaills->game_rules);
+        $getCategory = CompetitionCategory::all();
+        $getCompetitionForm = json_decode($getCompetionDetaills->register_form);
+        $encorededJson = json_encode($getCompetitionForm);
+
+        return view('frontend.user.orgz_edit_competition',[
+            'competition_details' => $getCompetionDetaills,
+            'game_rules' => $getGameRules,
+            'get_category' => $getCategory,
+            'form_data' => $encorededJson,
         ]);
     }
 
