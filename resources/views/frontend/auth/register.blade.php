b4c12266af86fafc135df941c1b0449577e4ff5d
Zajjith Vedha <zajjith@gmail.com>
2021-10-29 09:31:45 +0530

    git issues fixed

--- resources/views/frontend/auth/register.blade.php
+++ resources/views/frontend/auth/register.blade.php
@@ -10,7 +10,7 @@
                 <div class="layer">
                     <h4>Compete Smarter,</h4>
                     <p>AQOSE is much better when you have an Account</p>
-                    <p>Get your self a one</p>
+                    <p>Get yourself an account now</p>
                 </div>
             </div>
             <div class="rightSide">
@@ -38,21 +38,27 @@
                         <input type="password" class="form-control" id="exampleInputPassword1" name="password_confirmation" placeholder="Password" data-toggle="password" required>
                     </div>
 
-                    <div class="form-check">
+                    <div class="form-check mb-4">
                         <input type="checkbox" class="form-check-input" id="exampleCheck1" name="">
                         <label class="form-check-label" for="exampleCheck1">By signing up agree with <a>Terms and conditions.</a> </label>
                     </div>
 
-                    @if(config('access.captcha.registration'))
+                    <div class="row justify-content-center mb-4">
+                        <div class="col-10 text-center">
+                            <div class="g-recaptcha" data-callback="checked" data-sitekey="6Lel4Z4UAAAAAOa8LO1Q9mqKRUiMYl_00o5mXJrR" style="display: inline-block;"></div>
+                        </div>
+                    </div>
+
+                    <!-- @if(config('access.captcha.registration'))
                         <div class="row">
                             <div class="col">
                                 @captcha
                                 {{ html()->hidden('captcha_status', 'true') }}
-                            </div><!--col-->
-                        </div><!--row-->
-                    @endif
+                            </div>
+                        </div>
+                    @endif -->
                     <div class="theameRow">
-                        <button type="submit" class="btn btn-primary">Register</button>
+                        <button type="submit" class="btn btn-primary submit-btn" disabled>Register</button>
                         <h6>or <a href="{{route('frontend.auth.login')}}">Sign In</a></h6>
                     </div>
                 {{ html()->form()->close() }}
@@ -65,4 +71,12 @@
     @if(config('access.captcha.registration'))
         @captchaScripts
     @endif
+
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+    <script>
+        function checked() {
+        $('.submit-btn').removeAttr('disabled');
+    };
+    </script>
 @endpush
